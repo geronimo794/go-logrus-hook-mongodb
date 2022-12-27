7d878e344996484d9ef049882d00abee4a691bc8
Ach Rozikin@MacBook <geronimo794@gmail.com>
2022-12-28 00:40:41 +0700

    init: Initial release

--- mongolog.go
+++ mongolog.go
@@ -1,5 +1,52 @@
 package mongolog
 
-func NewMongolog() {
+import (
+	"context"
+	"fmt"
 
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewHookConnectionString(cs string, db string, coll string) (*hook, error) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(cs))
+	if err != nil {
+		return nil, err
+	}
+
+	return &hook{c: client.Database(db).Collection(coll)}, nil
+}
+
+/**
+* Hook struct for Logrus hook interface
+**/
+type hook struct {
+	c *mongo.Collection
+}
+
+func (h *hook) Fire(entry *logrus.Entry) error {
+	data := make(logrus.Fields)
+	data["level"] = entry.Level.String()
+	data["time"] = entry.Time
+	data["message"] = entry.Message
+
+	for k, v := range entry.Data {
+		if errData, isError := v.(error); logrus.ErrorKey == k && v != nil && isError {
+			data[k] = errData.Error()
+		} else {
+			data[k] = v
+		}
+	}
+	_, err := h.c.InsertOne(context.TODO(), data)
+
+	if err != nil {
+		return fmt.Errorf("failed to save log: %v", err)
+	}
+
+	return nil
+}
+
+func (h *hook) Levels() []logrus.Level {
+	return logrus.AllLevels
 }
---
+++ mongolog_test.go
@@ -0,0 +1,61 @@
+package mongolog_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/geronimo794/go-mongolog"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+
+var connectionString string
+var db string
+var coll string
+
+func init() {
+	// Load .env data put to os environment
+	if err := godotenv.Load(); err != nil {
+		panic("No .env file found")
+	}
+
+	/**
+	* Connection variable definition
+	**/
+	// Connection string
+	connectionString = os.Getenv("MONGO_DB_CONNECTION_STRING")
+
+	// Per variable definition
+	var mongoHost = os.Getenv("MONGO_DB_HOST_NATIVE")
+	var mongoUsername = os.Getenv("MONGO_DB_USERNAME")
+	var mongoPassword = os.Getenv("MONGO_DB_PASSWORD")
+	var mongoPort = os.Getenv("MONGO_DB_PORT")
+
+	// If mongo db connection string is empty then create the connection from variabel
+	if len(connectionString) == 0 {
+		connectionString = "mongodb://" + mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort + "/?retryWrites=true&w=majority"
+	}
+
+	// Set test db and collection
+	db = "db-test"
+	coll = "collection-test"
+}
+func TestNewHookConnectionString_Success(t *testing.T) {
+	log := logrus.New()
+	hook, err := mongolog.NewHookConnectionString(connectionString, db, coll)
+	if err == nil {
+		log.Hooks.Add(hook)
+	} else {
+		fmt.Print(err)
+	}
+
+	log.WithFields(logrus.Fields{
+		"name": "Ach Rozikin",
+	})
+
+	log.Warn("Warning log")
+	log.Trace("Trace log")
+	log.Error("Error log")
+
+}
